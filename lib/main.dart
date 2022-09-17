b1898df20382dcd5b2d619f28288e620eb1c1b60
Cao Minh Tâm <tamb1910136@student.ctu.edu.vn>
2022-09-18 01:35:36 +0700

    Xay dung trang chi tiet san pham

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:myshop/models/product.dart';
+import 'ui/products/product_detail_screen.dart';
+import 'ui/products/products_manager.dart';
+import 'ui/products/product_overview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +21,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple,)
       .copyWith(secondary: Colors.deepOrange,),
     ),
-      home: Container (color: Colors.green,),
+      home: const SafeArea(
+        child: ProductOverviewScreen(),
+      ),
     );
   }
 }
